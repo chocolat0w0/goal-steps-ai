d5f790bed395fd7466255839730f344b050b61ea
chocolat0w0 <2049423+chocolat0w0@users.noreply.github.com>
2025-09-09 01:07:57 -0700

    ビルド成功

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
